9a888be084285aa4e7a943508a3da067111efb38
kirakira0 <kiratoal0@gmail.com>
2019-09-23 19:10:09 -0700

    fixed platform issues

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@
 public class Player : MonoBehaviour {
 
     public Vector3 jump;
+    public Vector3 stop; 
     //public Vector3 dash; 
     public float jumpForce = 2.0f;
     public bool isGrounded;
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour {
     void Start(){
         player = this.GetComponent<Rigidbody2D>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
+        stop = new Vector3(0.0f, 0.0f, 0.0f); 
         //dash = new Vector3(2.0f, 1.0f, 0.0f); 
     }
 
@@ -22,14 +24,19 @@ public class Player : MonoBehaviour {
     }
 
     void Update(){
-        
+        Debug.Log(isGrounded); 
         if(Input.GetKeyDown(KeyCode.Z) && isGrounded){
+            //player.velocity = Vector3.zero; 
+            //player.AddForce(stop); 
             player.AddForce(jump * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
         }
+
         // if(Input.GetKeyDown(KeyCode.X)){
         //     player.AddForce(dash * jumpForce, ForceMode2D.Impulse);
         // }
     }
+
+    
     
 }
