e5e033cbb2514125be82a0f7a3a43b40faeba871
kirakira0 <kiratoal0@gmail.com>
2019-10-10 14:29:56 -0700

    created initial hail spawner

---
+++ Assets/HailSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HailSpawner : MonoBehaviour
+{
+    public GameObject hail; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(hail, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
