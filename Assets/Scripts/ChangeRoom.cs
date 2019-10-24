1bc665fe450420ee3c09194d5402aed43eba0f01
kirakira0 <kiratoal0@gmail.com>
2019-10-23 18:08:28 -0700

    created an implemented script that allows the player to move from scene to scene

---
+++ Assets/Scripts/ChangeRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeRoom : MonoBehaviour
+{
+    public string targetRoom; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(targetRoom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
