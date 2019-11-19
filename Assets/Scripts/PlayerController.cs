f9b89d53af8d2f8a0787e88118ae4015fb052e71
kirakira0 <kiratoal0@gmail.com>
2019-11-19 00:06:25 -0800

    Updated player controller for smoother motion and better collision detection.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed; 
+    public float jumpForce;
+    
+    private Rigidbody2D playerRigidbody;
+
+    //variables for ground collision
+    public bool grounded; 
+    public LayerMask whatIsGround; 
+    private Collider2D playerCollider;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>(); 
+        playerCollider = GetComponent<Collider2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, whatIsGround); 
+        //playerRigidbody.velocity = new Vector2(moveSpeed, playerRigidbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.UpArrow) && grounded) {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+        }
+    }
+}
