90d75dbd673ef6fb65dab8e54e3412b6410278cf
kirakira0 <kiratoal0@gmail.com>
2019-11-19 18:46:36 -0800

    Randomized distance between platforms.

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -6,9 +6,12 @@ public class PlatformGenerator : MonoBehaviour
 {
     public GameObject platform; 
     public Transform generationPoint; 
-    public float distanceBetween; 
     
     private float platformWidth; 
+    
+    private float distanceBetween; 
+    public float distanceBetweenMin;
+    public float distanceBetweenMax; 
 
     void Start()
     {
@@ -20,6 +23,7 @@ public class PlatformGenerator : MonoBehaviour
     {
         //create a new platform
         if (transform.position.x < generationPoint.position.x) {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
             Instantiate(platform, transform.position, transform.rotation); 
         }
